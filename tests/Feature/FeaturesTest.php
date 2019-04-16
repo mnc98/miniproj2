a6edb9eae5f11009f546fd3184399c82b1281f85
mnc98 <mncohen45@gmail.com>
2019-04-16 19:50:44 -0400

    1.  Write a feature test to check if the register return a 200 status code.

--- tests/Feature/FeaturesTest.php
+++ tests/Feature/FeaturesTest.php
@@ -9,10 +9,18 @@ use \App\User;
 class FeatureTest extends TestCase
 {
 
+    public function testReturnRegisterTest()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
     public function testReturnLoginTest()
     {
         $response = $this->get('/login');
 
         $response->assertStatus(200);
     }
+
 }
