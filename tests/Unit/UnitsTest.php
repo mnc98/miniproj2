19f75a700c2e774ab2f7190b93a00b41cf7af3c4
mnc98 <mncohen45@gmail.com>
2019-04-17 16:41:57 -0400

    8.  Create a unit test to count the number of records inserted by the database seed.  i.e. $userCount = 50

--- tests/Unit/UnitsTest.php
+++ tests/Unit/UnitsTest.php
@@ -27,4 +27,14 @@ class UnitsTest extends TestCase
         $user = User::find(mt_rand(1,50));
         $this->assertTrue($user->delete());
     }
+
+    public function testUserCountTest()
+    {
+        $user = User::all();
+        $userCount = 50;
+        $count = $user->count();
+        $this->assertEquals($userCount,$count);
+
+
+    }
 }
